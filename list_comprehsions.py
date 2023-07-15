39c23bc2ffa09d00cf24981a4ebf268ed5ea39d8
Kiran Bansal <136809550+Kiran-bansal@users.noreply.github.com>
2023-07-15 13:46:25 +0530

    Update list_comprehsions.py

--- list_comprehsions.py
+++ list_comprehsions.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     l=[]
     for i in range(x+1):
         for j in range(y+1):
-            for k in range(x+1):
+            for k in range(z+1):
                 if i+j+k==n:
                     continue
                 l.append([i,j,k])
