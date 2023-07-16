270e8fe53ee9d9d2513ba6e461a8155a092187db
Kiran Bansal <136809550+Kiran-bansal@users.noreply.github.com>
2023-07-16 14:11:04 +0530

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    N = int(input())
+    l = []
+    for i in range(N):
+        s = list(input().split())
+        if s[0]=='insert':
+            l.insert(int(s[1]),int(s[2]))
+        if s[0]=='remove':
+            l.remove(int(s[1]))
+        if s[0]=='append':
+            l.append(int(s[1]))
+        if s[0]=='sort':
+            l.sort()
+        if s[0]=='pop':
+            l.pop()
+        if s[0]=='reverse':
+            l.reverse()     
+        if s[0]=='print':
+            print(l)
