93ce575e90c187743a61a96e1c2b3bb9d5bea21e
Kiran Bansal <136809550+Kiran-bansal@users.noreply.github.com>
2023-07-16 13:49:07 +0530

    Create nested_list.py

---
+++ nested_list.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+            scores = []
+            for _ in range(int(input())):
+                name = input()
+                score = float(input()) 
+                scores.append([name,score])
+            
+            min_score = min(scores, key = lambda x: x[1])
+
+            scores = [score for score in scores if score[1] > min_score[1]]
+            
+            second_min_score = min(scores, key = lambda x: x[1])
+            
+            scores = [score for score in scores if score[1] == second_min_score[1]]
+            
+            scores.sort(key = lambda x: x[0])
+            
+            for score in scores:
+                print(score[0])
