3fd2cbfccb6f681dc85d276871deac2dc6a9bca0
Kiran Bansal <136809550+Kiran-bansal@users.noreply.github.com>
2023-07-21 14:22:38 +0530

    Create dynamicarray.py

---
+++ dynamicarray.py
@@ -0,0 +1,23 @@
+def dynamicArray(n, queries):
+
+    last_answer = 0
+    seq = [[] for _ in range(n)]
+
+    for q in queries:
+        op, x, y = q
+
+        if op == 1:
+            seq[(x ^ last_answer) % n].append(y)
+        elif op == 2:
+            s = seq[(x ^ last_answer) % n]
+            last_answer = s[y % len(s)]
+            print(last_answer)
+
+
+if __name__ == '__main__':
+    n, q = map(int, input().split())
+    queries = []
+    for _ in range(q):
+        queries.append(list(map(int, input().split())))
+
+    dynamicArray(n, queries)
